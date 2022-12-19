36a6fef72b0bf28ec01539cf2e0cc2a04bc6face
RaphaelBirkl1991 <raphael.birkl@gmail.com>
2022-12-19 09:53:50 +0100

    Mein Branch

---
+++ src/test/java/SpringThymeleaf/SpringUebung/SpringUebungApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringThymeleaf.SpringUebung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUebungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
